ad896661e9e1911c28c725a1ae2c0a55d6675947
vchchuang <vchchuang@gmail.com>
2014-05-14 23:48:34 +0800

    add ChibiOS-RT

---
+++ testhal/STM32F4xx/DMA_STORM/.dep/crt0.o.d
@@ -0,0 +1,22 @@
+build/obj/crt0.o: ../../../os/ports/GCC/ARMCMx/crt0.c \
+ /usr/local/csl/gcc-arm-none-eabi-4_8-2013q4/lib/gcc/arm-none-eabi/4.8.3/include/stdint.h \
+ /usr/local/csl/gcc-arm-none-eabi-4_8-2013q4/arm-none-eabi/include/stdint.h \
+ /usr/local/csl/gcc-arm-none-eabi-4_8-2013q4/arm-none-eabi/include/_ansi.h \
+ /usr/local/csl/gcc-arm-none-eabi-4_8-2013q4/arm-none-eabi/include/newlib.h \
+ /usr/local/csl/gcc-arm-none-eabi-4_8-2013q4/arm-none-eabi/include/sys/config.h \
+ /usr/local/csl/gcc-arm-none-eabi-4_8-2013q4/arm-none-eabi/include/machine/ieeefp.h \
+ /usr/local/csl/gcc-arm-none-eabi-4_8-2013q4/arm-none-eabi/include/sys/features.h
+
+/usr/local/csl/gcc-arm-none-eabi-4_8-2013q4/lib/gcc/arm-none-eabi/4.8.3/include/stdint.h:
+
+/usr/local/csl/gcc-arm-none-eabi-4_8-2013q4/arm-none-eabi/include/stdint.h:
+
+/usr/local/csl/gcc-arm-none-eabi-4_8-2013q4/arm-none-eabi/include/_ansi.h:
+
+/usr/local/csl/gcc-arm-none-eabi-4_8-2013q4/arm-none-eabi/include/newlib.h:
+
+/usr/local/csl/gcc-arm-none-eabi-4_8-2013q4/arm-none-eabi/include/sys/config.h:
+
+/usr/local/csl/gcc-arm-none-eabi-4_8-2013q4/arm-none-eabi/include/machine/ieeefp.h:
+
+/usr/local/csl/gcc-arm-none-eabi-4_8-2013q4/arm-none-eabi/include/sys/features.h:
